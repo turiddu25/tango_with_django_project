68e92a4a7ddf521bfce7e232d90083c480a50093
Colin Nardo <2795201n@student.gla.ac.uk>
2024-02-07 22:33:15 +0000

    chapter 10 done

--- rango/views.py
+++ rango/views.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from datetime import datetime
 
 
 
@@ -26,17 +26,23 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     
+    visitor_cookie_handler(request)
 
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Colin.'}
+    visitor_cookie_handler(request)
+    visits = request.session['visits']
+    context_dict = {'boldmessage': 'This tutorial has been put together by Colin.', 'visits': visits}
     # prints out whether the method is a GET or a POST
     print(request.method)
     # prints out the user name, if no one is logged in it prints `AnonymousUser`
-    print(request.user)
-    return render(request, 'rango/about.html', {})
+    print(request.user)    
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -239,9 +245,39 @@ def restricted(request):
 
 # Use the login_required() decorator to ensure only those logged in can
 # access the view.
-@login_required
+@login_required 
 def user_logout(request):
     # Since we know the user is logged in, we can now just log them out.
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
+
+
+
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
+
